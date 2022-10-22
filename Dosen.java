c715a008d057bd5e1645acde9657291dbd924c97
Juardi-7 <114724288+Juardi-7@users.noreply.github.com>
2022-10-22 20:00:29 +0700

    Add files via upload

---
+++ Dosen.java
@@ -0,0 +1,14 @@
+
+public class Dosen extends Mahasiswa{
+    String nama;
+
+    Dosen(String nama){
+        super(nama);
+        this.nama = nama;
+    }
+
+   
+    void info(){
+        System.out.println("Nama Dosen \t\t : " + this.nama);
+    }
+}
---
+++ Matkul.java
@@ -0,0 +1,12 @@
+public class Matkul extends Dosen{
+    String nama;
+
+    Matkul(String nama){
+        super(nama);
+        this.nama = nama;
+    }
+
+    void info(){
+        System.out.println("Mata Kuliah \t : " + this.nama);
+    }
+}
